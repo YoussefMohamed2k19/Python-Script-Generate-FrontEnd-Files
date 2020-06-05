0172ac5a1a20d9577999dddd03f97b66f1742e18
YoussefMohamed2k19 <elpopyoussef@gmail.com>
2020-06-05 09:12:12 +0200

    first commit

---
+++ Create Sample Frontend Enviroment.py
@@ -0,0 +1,20 @@
+import os
+
+# Create target Directory if don't exist
+dirName = ['css','js', 'img']
+
+for string in dirName:
+    if not os.path.exists(string):
+        os.mkdir(string)
+        print (string + " Created")
+    else:
+        print (string +" Exist")
+
+filename = ['index.html', 'style.css', 'script.js']
+open(filename[0], 'w').close()
+
+os.chdir("./css")
+open(filename[1], 'w').close()
+
+os.chdir("../js")
+open(filename[2], 'w').close()
