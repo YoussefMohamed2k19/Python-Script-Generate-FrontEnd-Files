e8f967cac53afb1527eb5516f9a65e573a682175
YoussefMohamed2k19 <elpopyoussef@gmail.com>
2020-06-24 00:28:24 +0200

    First Commit

--- Create Sample Frontend Enviroment.py
+++ Create Sample Frontend Enviroment.py
@@ -1,18 +1,35 @@
 import os
+import emoji
+
+while True:             # Loop continuously
+  projectName=str(input("Enter The Project Name: "))
+  # Get the input
+  if not os.path.exists(projectName):
+    os.mkdir(projectName)
+    print ("\n" + "Your Project Folder Was Created..." + "\n")
+    break
+  else:
+    print ("\n" + "This Project Name Already Used, Try Again!"+ "\n")
+    pass   
+
+# Navigate to Project Folder to Make new files
+os.chdir("./"+ projectName )
 
 # Create target Directory if don't exist
-dirName = ['css','js', 'img']
-
+dirName = ['Css','Js', 'Img']
 for string in dirName:
     if not os.path.exists(string):
         os.mkdir(string)
-        print (string + " Created")
+        print (string + " Folder Created...")
     else:
-        print (string +" Exist")
+        print ("Sorry" + string +" Exist!")
 
 # Create target files if don't exist
 filename = ['index.html', 'style.css', 'script.js']
+
 open(filename[0], 'w').close()
+
+# Bootstrap start template
 str = """<!doctype html>
 <html lang="en">
   <head>
@@ -22,7 +39,7 @@ str = """<!doctype html>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="Css/style.css">
     <title>Hello, world!</title>
   </head>
   <body>
@@ -33,11 +50,18 @@ str = """<!doctype html>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="Js/script.js" type="text/javascript"></script>
   </body>
 </html>"""
 open(filename[0],"w").write(str)
-os.chdir("./css")
+
+os.chdir("./Css")
 open(filename[1], 'w').close()
 
-os.chdir("../js")
+os.chdir("../Js")
 open(filename[2], 'w').close()
+
+# 🎉 Finished msg 🎉 
+print("\n" + emoji.emojize('Done:tada:!', use_aliases=True))
+
+os.system("pause")
